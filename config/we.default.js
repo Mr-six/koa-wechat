c531e9443d92fa839d0fd6937ebfeebb453f90fa
mr-six <582497914@qq.com>
2017-09-17 10:15:45 +0800

    add weconfig defualt

---
+++ config/we.default.js
@@ -0,0 +1,8 @@
+module.exports = {
+    device_info: '001',  // 设备编号
+    qr_url: 'weixin://wxpay/bizpayurl?',  // 二维码生成前缀
+    url: 'https://api.mch.weixin.qq.com',  // 微信 api 前缀
+    appid: 'wxd678efh567hg6787',  // 公众号 id
+    mch_id: '1230000109',   // 微信支付分配的商户号
+    key: '1cb9cea97f6675eb7217e0da48b97dab',  // 32位 key
+  }
