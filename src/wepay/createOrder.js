cbaf0f84491d40883a2b0d0cb0c51260cca95efd
miao <582497915@qq.com>
2017-09-20 17:57:43 +0800

    更改函数

--- src/wepay/createOrder.js
+++ src/wepay/createOrder.js
@@ -1,7 +1,7 @@
 const { we } = require('../../config')
 const $ = require('../utils')
 
-async function createO (body, ctx) {
+function createO (body, ctx) {
   // ip　判断
   let regip = /(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}/
   let ip = ctx.request.ip
