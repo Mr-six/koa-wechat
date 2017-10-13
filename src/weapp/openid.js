8393e9e7faf5323e1060df324fecaa42f1630d55
Mr-six <582497915@qq.com>
2017-10-13 16:58:49 +0800

    mofigy cofig

--- src/weapp/openid.js
+++ src/weapp/openid.js
@@ -6,11 +6,11 @@ module.exports = async function (ctx) {
     console.log(query)
     if (query.code) {
         let code = query.code
-        let {appid, appsecret} = we
+        let {appid_app, appsecret_app} = we
         let target = 'https://api.weixin.qq.com/sns/jscode2session?appid='
-            + appid +
+            + appid_app +
             '&secret='
-            + appsecret +
+            + appsecret_app +
             '&js_code='
             + code +
             '&grant_type=authorization_code'
