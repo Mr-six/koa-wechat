3a914fc9bf49b8c2033d375847f16db13afe4971
Mr-six <582497915@qq.com>
2017-10-16 16:28:20 +0800

    fix

--- src/wepay/index.js
+++ src/wepay/index.js
@@ -296,7 +296,7 @@ async function weappCreateOrder (ctx) {
    
     // 调用再次签名 https://pay.weixin.qq.com/wiki/doc/api/wxa/wxa_api.php?chapter=7_7&index=3
     let weappParams = {
-      appId: we.appid_app,
+      appid: we.appid_app,
       timeStamp: $.createTimestamp(),
       nonce_str: $.createNonceStr(),
       package: 'prepay_id=' + resO.xml.prepay_id,
